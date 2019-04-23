0bd3b7b970c26d2374df6c0b2a302e5086522305
Student <example@example.com>
2019-04-23 13:05:18 -0700

    add another test

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -6,8 +6,21 @@ namespace ToDoList.Tests
   [TestClass]
   public class ItemTest
   {
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item() //NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    } //typeof returns the data type of a class. GetType() returns the data type of a specific object. So is the newItem object is of the Item data type?
 
-    // Test methods will go here.
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.GetDescription();
+      Assert.AreEqual(description, result);
+    }
 
   }
 }
