e4e478b688ebfe83818652ad7fb1f03f1fe4f4af
Student <example@example.com>
2019-04-23 16:36:54 -0700

    add static method

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Tests
 {
@@ -7,25 +8,43 @@ namespace ToDoList.Tests
   public class ItemTest
   {
     [TestMethod]
-    public void ItemConstructor_CreatesInstanceOfItem_Item() //NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
-    {
-      Item newItem = new Item();
-      Assert.AreEqual(typeof(Item), newItem.GetType());
-    } //typeof returns the data type of a class. GetType() returns the data type of a specific object. So is the newItem object is of the Item data type?
-
-    [TestMethod]
-    public void GetDescription_ReturnsDescription_String()
+    public void GetAll_ReturnsItems_ItemList()
     {
       //Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
 
       //Act
-      string result = newItem.GetDescription();// GetDescription getter method
+      List<Item> result = Item.GetAll();
 
       //Assert
-      Assert.AreEqual(description, result);
-    }
+      CollectionAssert.AreEqual(newList, result);
+}
+    // [TestMethod]
+    // public void ItemConstructor_CreatesInstanceOfItem_Item() //NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    // {
+    //   Item newItem = new Item("test");
+    //   Assert.AreEqual(typeof(Item), newItem.GetType());
+    // } //typeof returns the data type of a class. GetType() returns the data type of a specific object. So is the newItem object is of the Item data type?
+    //
+    // [TestMethod]
+    // public void SetDescription_SetDescription_String()
+    // {
+    //   //Arrange
+    //   string description = "Walk the dog.";
+    //   Item newItem = new Item(description);
+    //
+    //   //Act
+    //   string updatedDescription = "Do the dishes";
+    //   newItem.SetDescription(updatedDescription);
+    //   string result = newItem.GetDescription();
+    //
+    //   //Assert
+    //   Assert.AreEqual(updatedDescription, result);
+    // }
 
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,16 +1,21 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
     private string _description;
+    private static List<Item> _instances = new List<Item> {};
 
-      public Item (string description)
-      {
-        _description = description;
-      }
-      public string GetDescription()
-      {
-      return _description
-      }
+    public string GetDescription()
+    {
+      return _description;
+    }
+
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+
   }
 }
