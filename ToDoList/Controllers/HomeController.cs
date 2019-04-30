fa57b0bc99aaade43c750453a1c4df98788e2903
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-04-29 19:52:12 -0700

    add loop and RedirectToAction()

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,16 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
 
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
+      //It will retrieve a list of all existing Items, passing in the entire list as the view's model
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
     }
 
     [Route("/items/new")]
@@ -19,12 +21,12 @@ namespace ToDoList.Controllers
       return View();
     }
 
-    [Route("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return View("Index", myItem);
-    }
+    [HttpPost("/items")]
+  public ActionResult Create(string description)
+  {
+    Item myItem = new Item(description);
+    return RedirectToAction("Index");
+  }
 
   }
 }
