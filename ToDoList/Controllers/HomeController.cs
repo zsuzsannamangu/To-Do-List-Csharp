1d37bc81a361e6220965df7d03895a0c0846c5d2
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-04-29 20:02:38 -0700

    add ItemsController.cs

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,32 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
-using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
-  public class HomeController : Controller
-  {
-
-    [HttpGet("/")]
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      //It will retrieve a list of all existing Items, passing in the entire list as the view's model
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
     }
-
-    [Route("/items/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
-    }
-
-    [HttpPost("/items")]
-  public ActionResult Create(string description)
-  {
-    Item myItem = new Item(description);
-    return RedirectToAction("Index");
-  }
-
-  }
 }
